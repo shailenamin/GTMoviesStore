0f52fc9b9532ab9847828eb23023d5c8b6a4dd80
shailenamin <shailensamin.00@gmail.com>
2025-01-30 16:44:32 -0500

    Worked on some of chapter 5
    Worked on data base

--- GTMoviesStore/urls.py
+++ GTMoviesStore/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 """
 URL configuration for GTMoviesStore project.
 
@@ -22,3 +24,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
