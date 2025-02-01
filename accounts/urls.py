a5879ced9537156f5152550d3c5c38c07c227c8c
FinEredium1 <fahmed71@gatech.edu>
2025-01-31 19:59:09 -0500

    Added login options

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('signup', views.signup, name='accounts.signup'),
+    path('login/', views.login, name='accounts.login'),
 ]
