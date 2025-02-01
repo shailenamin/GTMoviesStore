9a6b4578802b9e3261f67c85d838daa8e9f9fcae
FinEredium1 <fahmed71@gatech.edu>
2025-01-31 20:11:02 -0500

    Added logout options

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('signup', views.signup, name='accounts.signup'),
     path('login/', views.login, name='accounts.login'),
+    path('logout/', views.logout, name='accounts.logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from .forms import CustomUserCreationForm, CustomErrorList
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('home.index')
 
 
 def login(request):
