1f60a11b5016121d1ffb64df40f36e1dcd76166a
Toxicl-16 <34041366+Toxicl-16@users.noreply.github.com>
2025-02-03 17:04:05 -0500

    Finished Chapter 10

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Movie, Review
 from django.contrib.auth.decorators import login_required
 
@@ -29,6 +29,15 @@ def index(request):
     return render(request, 'movies/index.html',
                   {'template_data': template_data})
 
+def show(request, id):
+    movie = movies[id - 1]
+    reviews = Review.objects.filter(movie=movie)
+    template_data = {}
+    template_data['title'] = movie['name']
+    template_data['movie'] = movie
+    template_data['reviews'] = reviews
+    return render(request, 'movies/show.html', {'template_data': template_data})
+
 @login_required
 def create_review(request, id):
     if request.method == 'POST' and request.POST['comment'] != '':
@@ -41,3 +50,27 @@ def create_review(request, id):
         return redirect('movies.show', id=id)
     else:
         return redirect('movies.show', id=id)
+
+@login_required
+def edit_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if request.user != review.user:
+        return redirect('movies.show', id=id)
+    if request.method == 'GET':
+        template_data = {}
+        template_data['title'] = 'Edit Review'
+        template_data['review'] = review
+        return render(request, 'movies/edit_review.html', {'template_data' : template_data})
+    elif request.method == 'POST' and request.POST['comment'] != '':
+        review = Review.objects.get(id=review_id)
+        review.comment = request.POST['comment']
+        review.save()
+        return redirect('movies.show', id=id)
+    else:
+        return redirect('movies.show', id=id)
+
+@login_required
+def delete_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id, user=request.user)
+    review.delete()
+    return redirect('movies.show', id=id)
