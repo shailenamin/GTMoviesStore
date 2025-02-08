5b424335ed4a062209c434e9591712c7bf2ad70e
FinEredium1 <fahmed71@gatech.edu>
2025-02-07 22:12:34 -0500

    Changed database to MYSQL

--- GTMoviesStore/settings.py
+++ GTMoviesStore/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'GTMoviesStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'moviesstore',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
