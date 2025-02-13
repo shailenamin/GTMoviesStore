8626446f3469e040d305cdded2b8365bdeca7c43
Toxicl-16 <34041366+Toxicl-16@users.noreply.github.com>
2025-02-13 15:54:29 -0500

    Switch to SQLite

--- GTMoviesStore/settings.py
+++ GTMoviesStore/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'GTMoviesStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moviesstore',
-        'USER': 'root',
-        'PASSWORD': 'root',  # change this if needed
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
